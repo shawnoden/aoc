9caedb420169960baa9660cdb02b8dc480ddd610
Shawn Oden <shawnoden@users.noreply.github.com>
2023-12-12 11:10:24 -0600

    Add Day 9-12 templates

---
+++ 2023/sql/AOC_2023_Day10.sql
@@ -0,0 +1,45 @@
+/***** --- Day 10: Pipe Maze --- *****/
+/* https://adventofcode.com/2023/day/10 */
+/* SETUP */
+DECLARE @inp varchar(max) = '';
+
+/**** TEST */
+/*
+DECLARE @inp varchar(max) = ''
+*/
+
+--SELECT  @inp
+DECLARE @CRLF varchar(10) = char(13) + char(10) ;
+DECLARE @inStr varchar(max) = REPLACE(@inp,@CRLF,'|')
+
+--SELECT @inStr
+
+/* Instructions table. */
+DROP TABLE IF EXISTS #tmpInstructions
+CREATE TABLE #tmpInstructions (id int identity, instr varchar(max), numberList varchar(max) )
+
+INSERT INTO #tmpInstructions (instr)
+SELECT value FROM STRING_SPLIT(@inStr,'|')
+
+
+
+/* PART 1 */
+
+/* 
+Attempt 1: 
+*/
+
+/* PART 2 */
+
+/* 
+ATTEMPT 1:
+*/
+
+/*
+NOTES:
+
+Part 1:
+
+Part 2:
+
+*/
