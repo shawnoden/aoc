6ba1daba5eea6995834184a11f53a605940da3d9
Shawn Oden <shawnoden@users.noreply.github.com>
2023-12-18 06:28:28 -0600

    Day 9 Part 1

--- 2023/sql/AOC_2023_Day09.sql
+++ 2023/sql/AOC_2023_Day09.sql
@@ -1,12 +1,15 @@
 /***** --- Day 9: Mirage Maintenance --- *****/
 /* https://adventofcode.com/2023/day/9 */
 /* SETUP */
-DECLARE @inp varchar(max) = '';
-
-/**** TEST */
 /*
-DECLARE @inp varchar(max) = ''
+DECLARE @inp varchar(max) = '';
 */
+/**** TEST */
+
+DECLARE @inp varchar(max) = '0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45'
+
 
 --SELECT  @inp
 DECLARE @CRLF varchar(10) = char(13) + char(10) ;
@@ -20,6 +23,33 @@ CREATE TABLE #tmpInstructions (id int identity, instr varchar(max), numberList v
 
 INSERT INTO #tmpInstructions (instr)
 SELECT value FROM STRING_SPLIT(@inStr,'|')
+
+SELECT * FROM #tmpInstructions
+
+/* Instructions table. */
+DROP TABLE IF EXISTS #tmpNums
+CREATE TABLE #tmpNums (id int identity, instrNum int, instrNumPos int, instrRowNum int, instrBlockNum int, diffFromLast int )
+
+INSERT INTO #tmpNums (instrNum, instrNumPos, instrRowNum, instrBlockNum, diffFromLast)
+SELECT num, rn, id, 1, num - LAG(num) OVER (PARTITION BY id ORDER BY rn) AS diffFromLast
+FROM (
+	SELECT id, CAST(value AS int) AS num, ROW_NUMBER() OVER (PARTITION BY id ORDER BY (SELECT NULL)) AS rn
+		--, instr
+	FROM #tmpInstructions ti
+	CROSS APPLY STRING_SPLIT(instr, ' ')
+) s1
+
+SELECT * FROM #tmpNums WHERE instrRowNum = 1 ORDER BY instrBlockNum, instrNumPos
+
+INSERT INTO #tmpNums (instrNum, instrNumPos, instrRowNum, instrBlockNum, diffFromLast)
+SELECT diffFromLast, ROW_NUMBER() OVER (PARTITION BY instrRowNum ORDER BY instrNumPos) AS rn, instrRowNum, 4
+	, diffFromLast - LAG(diffFromLast) OVER (PARTITION BY instrRowNum, instrBlockNum ORDER BY instrNumPos)
+FROM #tmpNums
+WHERE instrBlockNum = 3
+
+
+
+
 
 
 
