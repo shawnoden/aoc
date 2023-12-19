35bfc3a8267f075e1cec7161a6260dd3c2b30013
Shawn Oden <shawnoden@users.noreply.github.com>
2023-12-19 16:35:59 -0600

    Day 9 Part 1 - Not Complete.

--- 2023/sql/AOC_2023_Day09.sql
+++ 2023/sql/AOC_2023_Day09.sql
@@ -4,7 +4,7 @@
 /*
 DECLARE @inp varchar(max) = '';
 */
-/**** TEST */
+/**** TEST ****/
 
 DECLARE @inp varchar(max) = '0 3 6 9 12 15
 1 3 6 10 15 21
@@ -12,7 +12,8 @@ DECLARE @inp varchar(max) = '0 3 6 9 12 15
 
 
 --SELECT  @inp
-DECLARE @CRLF varchar(10) = char(13) + char(10) ;
+SET @inp = REPLACE(@inp,char(13),'')
+DECLARE @CRLF varchar(10) = char(10) ;
 DECLARE @inStr varchar(max) = REPLACE(@inp,@CRLF,'|')
 
 --SELECT @inStr
@@ -24,40 +25,108 @@ CREATE TABLE #tmpInstructions (id int identity, instr varchar(max), numberList v
 INSERT INTO #tmpInstructions (instr)
 SELECT value FROM STRING_SPLIT(@inStr,'|')
 
-SELECT * FROM #tmpInstructions
+--SELECT * FROM #tmpInstructions
 
-/* Instructions table. */
-DROP TABLE IF EXISTS #tmpNums
-CREATE TABLE #tmpNums (id int identity, instrNum int, instrNumPos int, instrRowNum int, instrBlockNum int, diffFromLast int )
 
-INSERT INTO #tmpNums (instrNum, instrNumPos, instrRowNum, instrBlockNum, diffFromLast)
-SELECT num, rn, id, 1, num - LAG(num) OVER (PARTITION BY id ORDER BY rn) AS diffFromLast
-FROM (
+; WITH tmpInstr AS (
 	SELECT id, CAST(value AS int) AS num, ROW_NUMBER() OVER (PARTITION BY id ORDER BY (SELECT NULL)) AS rn
 		--, instr
 	FROM #tmpInstructions ti
 	CROSS APPLY STRING_SPLIT(instr, ' ')
+	WHERE ti.id = 1
+)
+
+
+/* Nums table. */
+DROP TABLE IF EXISTS #tmpNums
+CREATE TABLE #tmpNums (id int identity, instrNum int, instrNumPos int, instrRowNum int, instrBlockNum int, diffFromLast int )
+
+/* INSERT FIRST ROWS */
+INSERT INTO #tmpNums (instrNum, instrNumPos, instrRowNum, instrBlockNum, diffFromLast)
+SELECT num, rn, id, 1, num - LAG(num) OVER (PARTITION BY id ORDER BY rn) AS diffFromLast
+FROM (
+	SELECT id, CAST(value AS int) AS num, ROW_NUMBER() OVER (PARTITION BY id ORDER BY (SELECT NULL)) AS rn
+	FROM #tmpInstructions ti
+	CROSS APPLY STRING_SPLIT(instr, ' ')
 ) s1
 
-SELECT * FROM #tmpNums WHERE instrRowNum = 1 ORDER BY instrBlockNum, instrNumPos
 
-DECLARE @rowPass int = 1
-DECLARE @blockPass int = 2
-DECLARE @endState int = 99
+--SELECT * FROM #tmpNums
 
---WHILE @endState <> 0
---BEGIN
-	
---	INSERT INTO #tmpNums (instrNum, instrNumPos, instrRowNum, instrBlockNum, diffFromLast)
-	SELECT diffFromLast, ROW_NUMBER() OVER (PARTITION BY instrRowNum ORDER BY instrNumPos) AS rn, instrRowNum, @blockPass
-		, diffFromLast - LAG(diffFromLast) OVER (PARTITION BY instrRowNum, instrBlockNum ORDER BY instrNumPos)
-	FROM #tmpNums
-	WHERE instrBlockNum = @blockPass-1
-	
-	SET @endState = (SELECT sum(diffFromLast) FROM #tmpNums WHERE instrRowNum = @rowPass AND instrBlockNum = @blockPass )
-	
-	SET @blockPass = @blockPass+1
---END
+DECLARE @thisRow int = 1
+DECLARE @maxRow int = ( SELECT max(id) FROM #tmpInstructions )
+
+/* LOOP THROUGH ROWS */
+WHILE @thisRow <= @maxRow
+BEGIN
+	DECLARE @thisBlock int = 1
+
+	WHILE 1=1
+		BEGIN
+		INSERT INTO #tmpNums (instrNum, instrNumPos, instrRowNum, instrBlockNum, diffFromLast)
+		SELECT diffFromLast, ROW_NUMBER() OVER (PARTITION BY instrRowNum ORDER BY instrNumPos) AS rn, @thisRow, @thisBlock+1
+			, diffFromLast - LAG(diffFromLast) OVER (PARTITION BY instrRowNum, instrBlockNum ORDER BY instrNumPos)
+		FROM #tmpNums
+		WHERE instrBlockNum = @thisBlock
+			AND instrRowNum = @thisRow
+			--AND ( SELECT SUM(diffFromLast) FROM #tmpNums WHERE instrRowNum = @thisRow AND instrBlockNum = @thisBlock ) > 0
+
+		--SELECT @thisBlock, (SELECT SUM(diffFromLast) FROM #tmpNums WHERE instrRowNum = @thisRow AND instrBlockNum = @thisBlock)
+
+		IF (SELECT SUM(diffFromLast) FROM #tmpNums WHERE instrRowNum = @thisRow AND instrBlockNum = @thisBlock) <= 0
+		BEGIN
+			SET @thisBlock = 1
+		    break
+		END
+		ELSE 
+		BEGIN
+			SET @thisBlock = @thisBlock+1
+		END
+	END
+	SET @thisRow = @thisRow+1
+END
+
+SELECT instrNum, nextNum
+FROM (
+	SELECT * 
+		, LAG(instrNUM) OVER (PARTITION BY instrRowNum, instrBlockNum ORDER BY instrBlockNum DESC) AS prevDiff
+		, instrNum + LAG(instrNUM) OVER (ORDER BY instrBlockNum DESC) AS nextNum
+	FROM (
+		SELECT * 
+			, ROW_NUMBER() OVER ( PARTITION BY instrRowNum, instrBlockNum ORDER BY instrNumPos DESC) AS rn
+		FROM #tmpNums  
+		WHERE instrNum IS NOT NULL
+	) s1
+	WHERE s1.rn = 1
+) s2
+WHERE instrBlockNum = 1
+
+
+
+SELECT *
+	, LAG(instrNUM) OVER (PARTITION BY instrRowNum, instrBlockNum ORDER BY instrBlockNum DESC) AS prevDiff
+	, LEAD(instrNUM,1,0) OVER (PARTITION BY instrRowNum ORDER BY instrBlockNum DESC)
+	, instrNum + LAG(instrNUM,1,0) OVER (PARTITION BY instrRowNum ORDER BY instrBlockNum DESC) AS nextNum
+FROM (
+	SELECT * 
+	FROM (
+		SELECT * 
+			, ROW_NUMBER() OVER ( PARTITION BY instrRowNum, instrBlockNum ORDER BY instrNumPos DESC) AS rn
+		FROM #tmpNums  
+		WHERE instrNum IS NOT NULL
+	) s1
+	WHERE s1.rn = 1
+) s2
+WHERE instrBlockNum = 1
+
+
+	ORDER BY instrRowNum, instrBlockNum, instrNumPos
+
+
+
+
+--ORDER BY instrRowNum, instrBlockNum, instrNumPos
+
 
 
 
@@ -67,6 +136,7 @@ DECLARE @endState int = 99
 
 
 /* PART 1 */
+
 
 /* 
 Attempt 1: 
@@ -80,8 +150,17 @@ ATTEMPT 1:
 
 /*
 NOTES:
+This year has been full of parsing monsters. 
 
 Part 1:
+The first thing I need to do is to break these number sets down until they hit 0 results. Then I'll need
+to figure out how to backfill the missing numbers. 
+
+It sounds so easy. 
+
+....
+
+
 
 Part 2:
 
