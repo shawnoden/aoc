ed449dfdb8d339fed4c95b74b0e620cd6615c897
Shawn Oden <shawnoden@users.noreply.github.com>
2025-12-02 22:13:35 -0600

    AOC 2025 Day 2 - Part 1 Complete.

--- 2025/sql/AOC_2025_Day02.sql
+++ 2025/sql/AOC_2025_Day02.sql
@@ -2,13 +2,11 @@
 /* https://adventofcode.com/2025/day/2 */
 /* SETUP */
 
-/*
-DECLARE @inp varchar(max) = ''
-*/
+DECLARE @inp varchar(max) = '3737332285-3737422568,5858547751-5858626020,166911-236630,15329757-15423690,753995-801224,1-20,2180484-2259220,24-47,73630108-73867501,4052222-4199117,9226851880-9226945212,7337-24735,555454-591466,7777695646-7777817695,1070-2489,81504542-81618752,2584-6199,8857860-8922218,979959461-980003045,49-128,109907-161935,53514821-53703445,362278-509285,151-286,625491-681593,7715704912-7715863357,29210-60779,3287787-3395869,501-921,979760-1021259'
 
 /** TEST **/
 
-DECLARE @inp varchar(max) = '11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124'
+--DECLARE @inp varchar(max) = '11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124'
 
 --SELECT  @inp
 
@@ -19,7 +17,7 @@ DECLARE @inStr varchar(max) = REPLACE(@inp,',','|')
 
 /* Instructions table. */
 DROP TABLE IF EXISTS #tmpInstructions
-CREATE TABLE #tmpInstructions (id int identity, instr varchar(max), startNum int, endNum int, dupes varchar(max) )
+CREATE TABLE #tmpInstructions (id int identity, instr varchar(max), startNum bigint, endNum bigint, dupes varchar(max) )
 
 INSERT INTO #tmpInstructions (instr)
 SELECT value FROM STRING_SPLIT(@inStr,'|')
@@ -31,14 +29,62 @@ SET   startNum =  SUBSTRING(instr,1,CHARINDEX('-',instr)-1)
 	, endNum = SUBSTRING(instr,CHARINDEX('-',instr)+1, LEN(instr))
 
 
-SELECT * FROM #tmpInstructions
+--SELECT * FROM #tmpInstructions
 
 /**********************************************************************/
 
 /* PART 1 */
 
+/*** Create temp table to hold our numbers. ***/
+DROP TABLE IF EXISTS #tmpNumsInRange
+CREATE TABLE #tmpNumsInRange (instr varchar(max), num bigint, numLength bigint, FirstHalf varchar(2000), SecondHalf varchar(2000), matches bit)
+
+
+/*** Setup Loop ***/
+DECLARE @thisRow int = 1
+DECLARE @totalRows int = (SELECT max(id) FROM #tmpInstructions)
+
+
+WHILE @thisRow <= @totalRows
+BEGIN
+	/*** Create all numbers in range. ***/
+	DECLARE @startNum bigint, @endNum bigint, @theInp varchar(max) 
+	
+	SELECT @startNum = startNum, @endNum = endNum, @theInp = instr
+	FROM #tmpInstructions
+	WHERE id = @thisRow
+
+	INSERT INTO #tmpNumsInRange( instr,num, numLength, FirstHalf, SecondHalf )
+	SELECT @theInp, value , len(value)
+		, SUBSTRING(CAST(value AS varchar(2000)),1,len(value)/2)
+		, SUBSTRING(CAST(value AS varchar(2000)), (len(value)/2)+1, len(value))
+	FROM GENERATE_SERIES(@startNum,@endNum)
+	WHERE LEN(value)%2 = 0
+
+	/**** NEXT RECORD ****/
+	SET @thisRow = @thisRow+1
+END
+
+--SELECT * FROM #tmpInstructions
+
+--/*** Mark nums that can't match ***/
+--UPDATE #tmpNumsInRange
+--SET matches = 0
+--WHERE numlength%2 = 1
+
+/*** Mark other numbers as match or no match ***/
+UPDATE #tmpNumsInRange
+SET matches = 1
+WHERE FirstHalf = SecondHalf
+
+
+--SELECT count(*) FROM #tmpNumsInRange WHERE matches = 1
+SELECT * FROM #tmpNumsInRange WHERE numlength%2 = 0  ORDER BY num
+
+SELECT SUM(num) FROM #tmpNumsInRange WHERE matches = 1
+
 /* 
-?? = INCORRECT. 
+38437576669 = That's the right answer!
 */
 
 /**********************************************************************/
@@ -48,9 +94,6 @@ SELECT * FROM #tmpInstructions
 
 /* 
 ?? = INCORRECT, TOO ???. 
-
-
-Should be 6554!
 */
 
 /**********************************************************************/
@@ -60,12 +103,29 @@ NOTES:
 After a quick look at the data and the initial instruction, I need to parse out each block of number ranges, then parse out the start and end numbers.
 
 Part 1:
+NOTE: To use GENERATE_SERIES(), you have to be at least Compat Level 160 (SQL 2022+)
+
+After creating the horrible SQL loop again to iterate through the numbers generated, it was fairly simple to split the input strings into two 
+characters to compare. And if the numbers that needed to be generated came from a string with an odd number of characters, then they couldn't match, 
+so those were easy to eliminate right off the bat. I just needed to split the number string in half and compare the first half to the last half. 
+
+My method is horribly inefficient, since it generates a bunch of unused rows, but I can optimize that later if I want. 
+
+I also had to change to using bigint instead of just int for my numbers. The numbers were too big for ints in several rows. 
+
+-----------------------------------
+
+After going around in circles trying to figure out why I wasn't getting the right answer, I looked at the directions again and realized that I wasn't 
+supposed to count the number of invalid codes, but I was supposed to SUM THE VALUES of those codes. Duh.
+
+Part 1 is complete. 
 
 
 Part 2:
 
 
 Lesson Learned:
+AGAIN, READ THE DIRECTIONS CAREFULLY!
 
 */
 
